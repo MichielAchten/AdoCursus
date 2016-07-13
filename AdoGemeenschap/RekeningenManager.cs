da4988525616d0a195317ec7f6bcd02d37601de0
MichielAchten <michiel.achten@gmail.com>
2016-07-13 11:46:31 +0200

    CommandClass2

--- AdoGemeenschap/RekeningenManager.cs
+++ AdoGemeenschap/RekeningenManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Data.Common;
 
 namespace AdoGemeenschap
 {
@@ -22,6 +23,56 @@ namespace AdoGemeenschap
                     return comBonus.ExecuteNonQuery();
                 }
             }
+        }
+
+        public Boolean Storten(Decimal teStorten, string rekeningNr)
+        {
+            BankDbManager dbManager = new BankDbManager();
+            using (var conBank = dbManager.GetConnection())
+            {
+                using (var comStorten = conBank.CreateCommand())
+                {
+                    comStorten.CommandType = CommandType.StoredProcedure;
+                    comStorten.CommandText = "Storten";
+
+                    DbParameter parTeStorten = comStorten.CreateParameter();
+                    parTeStorten.ParameterName = "@teStorten";
+                    parTeStorten.Value = teStorten;
+                    parTeStorten.DbType = DbType.Currency;
+                    comStorten.Parameters.Add(parTeStorten);
+
+                    DbParameter parRekeningNr = comStorten.CreateParameter();
+                    parRekeningNr.ParameterName = "@RekeningNr";
+                    parRekeningNr.Value = rekeningNr;
+                    comStorten.Parameters.Add(parRekeningNr);
+
+                    conBank.Open();
+                    return comStorten.ExecuteNonQuery() != 0;
+                }
+            }
+            
+            //var dbManager = new BankDbManager();
+            //using (var conBank = dbManager.GetConnection())
+            //{
+            //    using (var comStorten = conBank.CreateCommand())
+            //    {
+            //        comStorten.CommandType = CommandType.Text;
+            //        comStorten.CommandText = "update Rekeningen set Saldo=saldo+@teStorten where RekeningNr=@RekeningNr";
+
+            //        DbParameter parTeStorten = comStorten.CreateParameter();
+            //        parTeStorten.ParameterName = "@teStorten";
+            //        parTeStorten.Value = teStorten;
+            //        comStorten.Parameters.Add(parTeStorten);
+
+            //        DbParameter parRekeningNr = comStorten.CreateParameter();
+            //        parRekeningNr.ParameterName = "@RekeningNr";
+            //        parRekeningNr.Value = rekeningNr;
+            //        comStorten.Parameters.Add(parRekeningNr);
+
+            //        conBank.Open();
+            //        return comStorten.ExecuteNonQuery() != 0;
+            //    }
+            //}
         }
     }
 }
