cb4bec5273d21283336373338dcc829bb80328e0
MichielAchten <michiel.achten@gmail.com>
2016-07-31 16:03:26 +0200

    EenWaardeUitDatabase1

--- AdoGemeenschap/RekeningenManager.cs
+++ AdoGemeenschap/RekeningenManager.cs
@@ -189,5 +189,35 @@ namespace AdoGemeenschap
             //    }
             //}
         }
+
+        public decimal SaldoRekeningRaadplegen(string rekeningNr)
+        {
+            var dbManager = new BankDbManager();
+            using (var conBank = dbManager.GetConnection())
+            {
+                using (var comSaldo = conBank.CreateCommand())
+                {
+                    comSaldo.CommandType = CommandType.StoredProcedure;
+                    comSaldo.CommandText = "SaldoRekeningRaadplegen";
+                    var parRekNr = comSaldo.CreateParameter();
+                    parRekNr.ParameterName = "@rekeningNr";
+                    parRekNr.Value = rekeningNr;
+                    comSaldo.Parameters.Add(parRekNr);
+                    conBank.Open();
+                    Object resultaat = comSaldo.ExecuteScalar();
+                    if (resultaat == null)
+                    {
+                        throw new Exception("Rekening bestaat niet");
+                    }
+                    else
+                    {
+                        return (Decimal)resultaat;
+                    }
+                }
+            }
+        }
+
+
+
     }
 }
