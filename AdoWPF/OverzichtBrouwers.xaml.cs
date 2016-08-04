0d18e94f34dab30af0acb8996248c2a659a92185
MichielAchten <michiel.achten@gmail.com>
2016-08-04 10:31:58 +0200

    Databinding4

--- AdoWPF/OverzichtBrouwers.xaml.cs
+++ AdoWPF/OverzichtBrouwers.xaml.cs
@@ -20,6 +20,7 @@ namespace AdoWPF
     /// </summary>
     public partial class OverzichtBrouwers : Window
     {
+        private CollectionViewSource brouwerViewSource;
         public OverzichtBrouwers()
         {
             InitializeComponent();
@@ -45,32 +46,72 @@ namespace AdoWPF
             brouwerViewSource.Source = manager.GetBrouwersBeginNaam(textBoxZoeken.Text);
         }
 
+        private void goToFirstButton_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
 
-        //private void VulDeGrid()
-        //{
-        //    CollectionViewSource brouwerViewSource = (CollectionViewSource)(this.FindResource("brouwerViewSource"));
-        //    var manager = new BrouwerManager();
-        //    brouwerViewSource.Source = manager.GetBrouwersBeginNaam(textBoxZoeken.Text);
-        //}
+        private void VulDeGrid()
+        {
+            CollectionViewSource brouwerViewSource = (CollectionViewSource)(this.FindResource("brouwerViewSource"));
+            var manager = new BrouwerManager();
+            brouwerViewSource.Source = manager.GetBrouwersBeginNaam(textBoxZoeken.Text);
+        }
 
-        //private void Window_Loaded(object sender, RoutedEventArgs e)
-        //{
-        //    VulDeGrid();
-        //    textBoxZoeken.Focus();
-        //}
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            VulDeGrid();
+            textBoxZoeken.Focus();
+        }
 
-        //private void buttonZoeken_Click(object sender, RoutedEventArgs e)
-        //{
-        //    VulDeGrid();
-        //}
+        private void buttonZoeken_Click(object sender, RoutedEventArgs e)
+        {
+            VulDeGrid();
+        }
 
-        //private void textBoxZoeken_KeyUp(object sender, KeyEventArgs e)
-        //{
-        //    if (e.Key == Key.Enter)
-        //    {
-        //        VulDeGrid();
-        //    }
-        //}
+        private void textBoxZoeken_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                VulDeGrid();
+            }
+        }
+
+        private void goToFirstButton_Click(object sender, RoutedEventArgs e)
+        {
+            brouwerViewSource.View.MoveCurrentToFirst();
+            goUpdate();
+        }
+
+        private void goToPreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void goToNextButton_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void goToLastButton_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void goUpdate()
+        {
+            goToPreviousButton.IsEnabled = !(brouwerViewSource.View.CurrentPosition == 0);
+            goToPreviousButton.IsEnabled = !(brouwerViewSource.View.CurrentPosition == 0);
+            goToNextButton.IsEnabled = !(brouwerViewSource.View.CurrentPosition == brouwerDataGrid.Items.Count - 1);
+            goToLastButton.IsEnabled = !(brouwerViewSource.View.CurrentPosition == brouwerDataGrid.Items.Count - 1);
+
+            if (brouwerDataGrid.Items.Count !=0)
+            {
+                if (brouwerDataGrid.SelectedItem != null)
+                {
+                    brouwerDataGrid.ScrollIntoView(brouwerDataGrid.SelectedItem);
+                }
+            }
+        }
     }
 }
